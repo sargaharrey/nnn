997f6e1726e38426f29a7577baacdac57405005e
Abdullah Albrawy <48991028+sargaharrey@users.noreply.github.com>
2022-11-24 08:10:58 -0800

    Update test.js

--- test.js
+++ test.js
@@ -78,4 +78,4 @@ app.post('/index/search', (req, res) => {
 // crawler('Cat videos', function (results) {
 // 	console.dir(results); //Outputs an array filled with cat videos.
 // });
-applisten(3000)
+app.listen(3000)
